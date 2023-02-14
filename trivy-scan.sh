07623c6777b39ffd3f0c2bb603ca8c9ce27a3b37
Ahmed Magdi <a.magdi888@gmail.com>
2023-02-14 18:31:22 +0200

    add Image Scanning

--- trivy-scan.sh
+++ trivy-scan.sh
@@ -5,9 +5,9 @@ ImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 trivy -q image  --timeout 50m0s --severity HIGH --exit-code 0  $ImageName
 trivy -q image  --timeout 50m0s --severity CRITICAL --exit-code 1  $ImageName
 
-exit-code=$?
+exit_code=$?
 
-if [[ $exit-code == 1 ]]
+if [[ $exit_code == 1 ]]
     then
         echo "Image Scanning failed, Vulnerabilities found"
         exit 1;
