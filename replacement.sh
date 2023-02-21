c3b5100d3d8da7b676d1354470c68c901496428d
Ahmed Magdi <a.magdi888@gmail.com>
2023-02-21 16:37:59 +0200

    add openapi lib

--- replacement.sh
+++ replacement.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 image=$(yq '.spec.template.spec.containers[].image' k8s_deployment_service.yaml)
-sed -i 's#${image}#${imageName}#g' k8s_deployment_service.yaml
+sed -i "s#$image#${imageName}#g" k8s_deployment_service.yaml
