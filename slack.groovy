fe4a021cc5b24ec9ea7acc5f6761a50ecd86773e
Ahmed Magdi <a.magdi888@gmail.com>
2023-02-24 18:23:46 +0200

    add slack

--- slack.groovy
+++ slack.groovy
@@ -18,4 +18,5 @@ def call(String buildStatus = 'STARTED') {
   def msg = "${buildStatus}: ${env.JOB_NAME} #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
 
   slackSend(color: color, message: msg)
+  return this
 }
